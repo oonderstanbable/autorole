d1df62870c8e634f731b4fd8c314469ad550e456
CharmingMother <nawhpongmaji1@gmail.com>
2019-01-09 20:34:15 -0600

    Update fun.py

--- cog_example/fun.py
+++ cog_example/fun.py
@@ -1,12 +1,18 @@
 import discord
 import asyncio
-
+from discord.ext import commands
 
 
 
 class Fun:
-  pass
+  def __init__(self,bot):
+    self.bot= bot
 
+
+    
+  @commands.command(pass_context=True):
+  async def fun(con):
+    await self.bot.say("This is fun!")
 
 
 def setup(bot):
