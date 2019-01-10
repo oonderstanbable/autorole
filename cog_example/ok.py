6237f62783a906f43b633208d0904001f99c20f8
CharmingMother <nawhpongmaji1@gmail.com>
2019-01-09 20:35:59 -0600

    Update ok.py

--- cog_example/ok.py
+++ cog_example/ok.py
@@ -1,3 +1,15 @@
 from discod.ext import commands
 import discord
 import asyncio
+
+class Ok:
+  def __init__(self,bot):
+    self.bot = bot
+    
+    
+  @commands.command(pass_context=True)
+  async def ok(con):
+    await self.bot.say("OK")
+
+def setup(bot):
+    bot.add_cog(Ok(bot))
